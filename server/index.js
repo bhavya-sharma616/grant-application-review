a4169f1aeeb1e1180d9a4cf42dcc9783ed0478f4
Bhavya Sharma <bhavya3720@gmail.com>
2026-08-29 22:37:53 +0530

    feat: add frontend login and API integration

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-
+const cors = require("cors");
 const express = require("express");
 const mongoose = require('mongoose');
 const protect = require("./middleware/authMiddleware");
@@ -12,6 +12,7 @@ const reviewRoutes = require("./routes/reviewRoutes");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/',(reqq,res)=>{
